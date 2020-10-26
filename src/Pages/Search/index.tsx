2e08691403827031be78a14d5245829a5f833ac0
Jean Santos <jeancbsan@gmail.com>
2020-10-26 20:39:11 -0300

    Correção handleSubmit

--- src/Pages/Search/index.tsx
+++ src/Pages/Search/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Navbar from '../../Core/Components/NavBar';
 import { makeRequest } from '../../Core/utils/requests';
 import ButtonIcon from '../../Core/Components/ButtonIcon/';
@@ -14,15 +14,6 @@ const Search = () => {
     const [user, setUser] = useState<User>();
     const [usuario, setUsuario] = useState(user?.name);
 
-    useEffect(() => {
-        setIsLoading(true);
-        makeRequest({ url: `${usuario}` })
-            .then(response => setUser(response.data))
-            .finally(() => {
-                setIsLoading(false);
-            });
-    }, [usuario]);
-
     const handleOnchange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setUsuario(event.target.value);
     }
@@ -30,14 +21,26 @@ const Search = () => {
     function handleOnclick() {
         setIsCliked(true);
     }
+
+    function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+
+        setIsLoading(true);
+        makeRequest({ url: `${usuario}` })
+            .then((Response) => setUser(Response.data))
+            .finally(() => {
+                setIsLoading(false);
+            });
+    }
     return (
         <>
             <Navbar />
 
             <div className="content-geral">
-                <h1 className="content-title"> 
-                    Encontre um perfil Github
-                </h1>
+                <form onSubmit={handleSubmit}>
+                    <h1 className="content-title">
+                        Encontre um perfil Github
+                    </h1>
                     <input
                         type="text"
                         placeholder="Usuario Github"
@@ -48,69 +51,71 @@ const Search = () => {
                     <div className="content-button">
                         <ButtonIcon text='Encontrar' onClick={handleOnclick} />
                     </div>
+                </form>
+            </div>
 
-                {isCLiked &&
-                    <div className='info-container'>
-                        {isLoading ? (
-                            <div className='cont-loader'>
-                                <div className='loader-esq'>
-                                    <ImageLoader />
-                                </div>
-                                <div className='loader-dir'>
-                                    <InfoLoader />
-                                </div>
+            {isCLiked && (
+                <div className='info-container'>
+                    {isLoading ? (
+                        <div className='cont-loader'>
+                            <div className='loader-esq'>
+                                <ImageLoader />
                             </div>
-                        ) : (
-                                <>
-                                    <div className="coluna1">
-                                        <img src={user?.avatar_url} className="container-img"></img>
-                                        <div className="button-profile_cont">
-                                            <a
-                                                target='_blank'
-                                                rel='noopener noreferrer'
-                                                href={`https://github.com/${usuario}`}
-                                            >
+                            <div className='loader-dir'>
+                                <InfoLoader />
+                            </div>
+                        </div>
+                    ) : (
+                            <>
+                                <div className="coluna1">
+                                    <img src={user?.avatar_url} className="container-img"></img>
+                                    <div className="button-profile-cont">
+                                        <a className="link"
+                                            target='_blank'
+                                            rel='noopener noreferrer'
+                                            href={`https://github.com/${usuario}`}
+                                        >
                                             <ButtonIcon
                                                 text="Ver Perfil" />
-                                            </a>
-                                        </div>
+                                        </a>
                                     </div>
-                                    <div className="coluna2">
-                                        <div className="linhas-topo">
-                                            <h1 className="container-total-repositorios">
-                                                Repositórios públicos: {user?.public_repos}
-                                            </h1>
-                                            <h1 className="container-total-repositorios">
-                                                Seguidores: {user?.followers}
-                                            </h1>
-                                            <h1 className="container-total-repositorios">
-                                                Seguindo: {user?.following}
-                                            </h1>                                            
-                                        </div>
-
-                                        <div className="linhas-bottom">
-                                            <h1 className="form-title">
-                                                Informações
-                                            </h1>
-                                            <h1 className="form-info">
-                                                    <strong>Empresa:</strong> {user?.company}
-                                            </h1>
-                                            <h1 className="form-info">
-                                                    <strong>Website/Blog:</strong> {user?.blog}
-                                            </h1>
-                                            <h1 className="form-info">
-                                                    <strong>Localidade:</strong> {user?.location}
-                                            </h1>
-                                            <h1 className="form-info">
-                                                    <strong>Membro desde:</strong> {user?.created_at}
-                                            </h1>
-                                        </div>
+                                </div>
+                                <div className="coluna2">
+                                    <div className="linhas-topo">
+                                        <h1 className="container-total-repositorios">
+                                            Repositórios públicos: {user?.public_repos}
+                                        </h1>
+                                        <h1 className="container-total-repositorios">
+                                            Seguidores: {user?.followers}
+                                        </h1>
+                                        <h1 className="container-total-repositorios">
+                                            Seguindo: {user?.following}
+                                        </h1>
                                     </div>
-                                </>      
 
-                            )}
-                    </div>}
-            </div>
+                                    <div className="linhas-bottom">
+                                        <h1 className="form-title">
+                                            Informações
+                                        </h1>
+                                        <h1 className="form-info">
+                                            <strong>Empresa:</strong> {user?.company}
+                                        </h1>
+                                        <h1 className="form-info">
+                                            <strong>Website/Blog:</strong> {user?.blog}
+                                        </h1>
+                                        <h1 className="form-info">
+                                            <strong>Localidade:</strong> {user?.location}
+                                        </h1>
+                                        <h1 className="form-info">
+                                            <strong>Membro desde:</strong> {user?.created_at}
+                                        </h1>
+                                    </div>
+                                </div>
+                            </>
+
+                        )}
+                </div>
+            )}  
         </>
     );
 };
